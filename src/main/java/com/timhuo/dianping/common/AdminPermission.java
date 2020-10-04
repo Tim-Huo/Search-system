d4ec534dd08b006b6781fa1cf4d2b3efd58da3b3
Tim-Huo <huowencheng@yeah.net>
2020-10-04 08:50:13 +0800

    上传管理员权限和切面相关代码

---
+++ src/main/java/com/timhuo/dianping/common/AdminPermission.java
@@ -0,0 +1,4 @@
+package com.timhuo.dianping.common;
+
+public @interface AdminPermission {
+}
---
+++ src/main/java/com/timhuo/dianping/common/ControllerAspect.java
@@ -0,0 +1,9 @@
+package com.timhuo.dianping.common;
+
+/**
+ * @description:
+ * @author: Tim_Huo
+ * @created: 2020/10/04 08:30
+ */
+public class ControllerAspect {
+}
